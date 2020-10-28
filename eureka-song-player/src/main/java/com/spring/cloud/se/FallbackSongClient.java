ce058614e5d675f0489d0876784b6d3def0414d8
evgeniy_skiba <eugene.s@it-dimension.com>
2020-10-28 14:39:14 +0200

    Feign demo. Added Circuit Breaker

--- eureka-song-player/src/main/java/com/spring/cloud/se/FallbackSongClient.java
+++ eureka-song-player/src/main/java/com/spring/cloud/se/FallbackSongClient.java
@@ -2,6 +2,9 @@ package com.spring.cloud.se;
 
 import org.springframework.stereotype.Component;
 
+/***
+ *  the logic that will be triggered when the servers are unavailable (th
+ */
 @Component
 public class FallbackSongClient implements SongClient {
 
